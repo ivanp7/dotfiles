8918de2ed79aa46fb071a19088da0711c130c051
Dmitry Olefir <dmitry.commits@gmail.com>
2018-06-13 22:46:41 +0000

    Update make-links.sh

--- make-links.sh
+++ make-links.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-cd `dirname $0`
-cd ..
+CONF_DIR=$(pwd)
 
-ln -s dotfiles/.aliases
-ln -s dotfiles/.bash_logout
-ln -s dotfiles/.bash_profile
-ln -s dotfiles/.bashrc
-ln -s dotfiles/.inputrc
+ln -sf $CONF_DIR/.aliases ~/
+ln -sf $CONF_DIR/.bash_logout ~/
+ln -sf $CONF_DIR/.bash_profile ~/
+ln -sf $CONF_DIR/.bashrc ~/
+ln -sf $CONF_DIR/.inputrc ~/
+ln -sf $CONF_DIR/.gitconfig ~/
+ln -sf $CONF_DIR/.gitignore_global ~/
 
-ln -s dotfiles/.gitconfig
-ln -s dotfiles/.gitignore_global
+mkdir -p ~/.ssh/
+ln -sf $CONF_DIR/.ssh/config ~/.ssh/
 
-ln -s ../dotfiles/.ssh/config .ssh/
+mkdir -p ~/.config/
+ln -sf $CONF_DIR/.config/ranger ~/.config/
+ln -sf $CONF_DIR/.config/neofetch ~/.config/
 
-ln -s ../dotfiles/.config/ranger .config/
-ln -s ../dotfiles/.config/neofetch .config/
+sudo ln -sf $CONF_DIR/.aliases /root/
+sudo ln -sf $CONF_DIR/.bash_logout /root/
+sudo ln -sf $CONF_DIR/.bash_profile /root/
+sudo ln -sf $CONF_DIR/.bashrc /root/
+sudo ln -sf $CONF_DIR/.inputrc /root/
+sudo ln -sf $CONF_DIR/.gitconfig /root/
+sudo ln -sf $CONF_DIR/.gitignore_global /root/
 
+sudo mkdir -p /root/.ssh/
+sudo ln -sf $CONF_DIR/.ssh/config /root/.ssh/
+
+sudo mkdir -p /root/.config/
+sudo ln -sf $CONF_DIR/.config/ranger /root/.config/
+sudo ln -sf $CONF_DIR/.config/neofetch /root/.config/
