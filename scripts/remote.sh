7c1e95ef5dce5ec4ecce093618fdff2d279e92d9
ivanp7 <ivanp7.git@gmail.com>
2018-09-17 01:50:33 +0300

    Swap 1st and 2nd parameters in remote.sh; drop -a argument from rsync in remote.sh

--- scripts/remote.sh
+++ scripts/remote.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-DESCRIPTION="$(grep "^$2 " ~/.known_ssh_hosts)"
+HOST="$1"
+
+DESCRIPTION="$(grep "^$HOST " ~/.known_ssh_hosts)"
 read -r COMPUTER USERNAME HOSTNAME PORT WAKEUP_PORT MAC_ADDRESS <<< "$DESCRIPTION"
 
 if [[ -z "$COMPUTER" ]]; then
-    echo Error: unknown host "$2"
+    echo Error: unknown host "$HOST"
     exit 2
 fi
 
-if [[ "$1" == "status" ]]; then
+MODE="$2"
+
+if [[ "$MODE" == "status" ]]; then
     COMMAND="nc -z $HOSTNAME $PORT"
-elif [[ "$1" == "wakeup" ]]; then
+elif [[ "$MODE" == "wakeup" ]]; then
     COMMAND="wol -p $WAKEUP_PORT -i $HOSTNAME $MAC_ADDRESS"
 
-elif [[ "$1" == "upload" ]]; then
-    COMMAND="rsync -avP ${@:5} -e 'ssh -p $PORT' $3 '$USERNAME@$HOSTNAME:$4'"
-elif [[ "$1" == "download" ]]; then
-    COMMAND="rsync -avP ${@:5} -e 'ssh -p $PORT' '$USERNAME@$HOSTNAME:$3' $4"
+elif [[ "$MODE" == "upload" ]]; then
+    COMMAND="rsync -vP ${@:5} -e 'ssh -p $PORT' $3 '$USERNAME@$HOSTNAME:$4'"
+elif [[ "$MODE" == "download" ]]; then
+    COMMAND="rsync -vP ${@:5} -e 'ssh -p $PORT' '$USERNAME@$HOSTNAME:$3' $4"
 
-elif [[ "$1" == "mount" ]]; then
+elif [[ "$MODE" == "mount" ]]; then
     COMMAND="sshfs $USERNAME@$HOSTNAME:$3 $4 -p $PORT -o reconnect"
-elif [[ "$1" == "unmount" ]]; then
+elif [[ "$MODE" == "unmount" ]]; then
     COMMAND="fusermount3 -u $3"
 
-elif [[ "$1" == "command" ]]; then
+elif [[ "$MODE" == "command" ]]; then
     SSH_FLAGS="-p $PORT"
     COMMAND="TERM=xterm-256color ssh $SSH_FLAGS $USERNAME@$HOSTNAME ${@:3}"
-elif [[ "$1" == "tunnel" ]]; then
+elif [[ "$MODE" == "tunnel" ]]; then
     LOCAL_PORT=$3
     if [[ -z "$LOCAL_PORT" ]]; then LOCAL_PORT=65535; fi
 
@@ -34,7 +38,7 @@ elif [[ "$1" == "tunnel" ]]; then
     COMMAND="TERM=xterm-256color ssh $SSH_FLAGS $USERNAME@$HOSTNAME"
 
 else
-    echo Error: unknown command "$1"
+    echo Error: unknown command "$MODE"
     exit 1
 fi
 
