e40b51700e464697721e76ad7bb22e4854b2b774
ivanp7 <ivanp7.git@gmail.com>
2018-11-01 23:23:49 +0300

    Decrease delay before calendar and todo-list update

--- .todo/update_todo.sh
+++ .todo/update_todo.sh
@@ -5,7 +5,7 @@ todo.sh list
 while true
 do
     inotifywait -qq -e move_self -e close_write $HOME/Org/todo/todo.txt
-    sleep 0.5
+    sleep 0.1
     clear
     echo
     todo.sh list
--- .when/update_calendar.sh
+++ .when/update_calendar.sh
@@ -5,7 +5,7 @@ when ci
 while true
 do
     inotifywait -qq -e delete_self -e close_write $HOME/Org/calendar
-    sleep 0.5
+    sleep 0.1
     clear
     echo
     when ci
