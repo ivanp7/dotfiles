6426295ef88138d7a15e7362e43f36be9caeb256
ivanp7 <ivanp7.git@gmail.com>
2018-11-03 18:40:15 +0300

    Add check for wallpaper existence in y-desktop.sh

--- scripts/y-desktop.sh
+++ scripts/y-desktop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-screen -c $HOME/.screenrc_nointerface yaft_wall $HOME/wallpapers/yaft.wallpaper
+if [ -f $HOME/wallpapers/yaft.wallpaper ]
+then screen -c $HOME/.screenrc_nointerface yaft_wall $HOME/wallpapers/yaft.wallpaper;
+else screen -c $HOME/.screenrc_nointerface yaft;
+fi
+
 clear
 
