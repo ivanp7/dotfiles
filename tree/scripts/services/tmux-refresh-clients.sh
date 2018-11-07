97cafa256819703ff26044e66e1d0d5d7accfdac
ivanp7 <ivanp7.git@gmail.com>
2018-11-08 02:43:19 +0300

    Make yaft redraw bug workaround working in the cases of updating tmux screens

--- tree/scripts/services/tmux-refresh-clients.sh
+++ tree/scripts/services/tmux-refresh-clients.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 UNUSED_VT=13
+TMUX_TMP_WINDOW=yaft-tmp-window
 
 TMUX_STATUS_INTERVAL=$(tmux show-options -g status-interval | cut -d' ' -f2)
 
-list_clients()
+list_yaft_clients()
 {
     tmux list-clients 2> /dev/null | grep yaft-256color
+}
+
+list_non_yaft_clients()
+{
+    tmux list-clients 2> /dev/null | grep -v yaft-256color
 }
 
 find_client()
@@ -16,44 +22,93 @@ find_client()
         local PTS=$(cat $HOME/.tmux_tmp/$1)
         if [[ -n "$PTS" ]]
         then
-            local CLIENT=$(list_clients | grep $PTS)
-            echo $CLIENT
+            local CLIENT=$(list_yaft_clients | grep $PTS)
+            echo -n "$CLIENT"
         fi
     fi
 }
 
 client_pts()
 {
-    echo $1 | cut -d':' -f1
+    echo -n "$1" | cut -d':' -f1
 }
 
 client_session()
 {
-    echo $1 | cut -d' ' -f2
+    echo -n "$1" | awk '{print $2}'
+}
+
+restore_client()
+{
+    # destroy the empty temporary window if it's there
+    if [[ "$(tmux display-message -c $(client_pts "$1") -p '#W')" == $TMUX_TMP_WINDOW ]]
+    then
+        local session=$(client_session "$1")
+        echo "restore: $session"
+
+        # enable status bar updating
+        tmux set -t $session status-interval $TMUX_STATUS_INTERVAL
+        tmux set visual-activity on
+
+        tmux kill-window -t $session:$TMUX_TMP_WINDOW
+
+        sleep 0.3
+    fi
+
+    # refresh screen
+    tmux refresh-client -t $(client_pts "$1")
+}
+
+hide_client()
+{
+    # switch to the empty temporary window to suppress framebuffer redraws
+    if [[ "$(tmux display-message -c $(client_pts "$1") -p '#W')" != $TMUX_TMP_WINDOW ]]
+    then
+        local session=$(client_session "$1")
+
+        # disable screen updating
+        tmux set -t $session status-interval 0
+        tmux set visual-activity off
+
+        tmux new-window -t $session -n $TMUX_TMP_WINDOW 'bash --norc'
+    fi
+}
+
+refresh_tty()
+{
+    # switch to an unused virtual terminal, wait a little and return
+    sleep 0.3
+    sudo chvt $UNUSED_VT
+    sleep 0.3
+    sudo chvt ${1:3}
 }
 
 while true
 do
+    # wait for TTY switch
     inotifywait -qq -e modify /sys/class/tty/tty0/active
     sleep 0.1
+    # get current tty name
     TTY=$(cat /sys/class/tty/tty0/active)
+    # get tmux client under yaft belonging to current tty
     CLIENT=$(find_client $TTY)
 
+    # we're in yaft, restore the tmux client
     if [[ -n $CLIENT ]]
     then
-        tmux set -t $(client_session $CLIENT) status-interval $TMUX_STATUS_INTERVAL
-        sleep 0.3
-        tmux refresh-client -t $(client_pts $CLIENT)
+        restore_client "$CLIENT"
+
+    # we're on bare tty, hide all yaft tmux clients
     elif [[ ! -f "$HOME/.tmux_tmp/$TTY" ]]
     then
-        for client in $(list_clients)
-        do
-            tmux set -t $client status-interval 0
-        done
-
-        sudo chvt $UNUSED_VT
-        sleep 0.3
-        sudo chvt ${TTY:3}
+        IFS=$'\n'
+        for client in $(list_yaft_clients); do hide_client "$client"; done
+        refresh_tty $TTY
+        
+    # we're in X, restore all non-yaft tmux clients
+    else
+        IFS=$'\n'
+        for client in $(list_non_yaft_clients); do restore_client "$client"; done
     fi
 
     PREV_TTY=$TTY
