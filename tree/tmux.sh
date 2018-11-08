455f3ffc5cb4718cbe2ca2161eca1b3d910933fb
ivanp7 <ivanp7.git@gmail.com>
2018-11-09 01:20:06 +0300

    Change default tmux session name to "default-USERNAME"

--- tree/tmux.sh
+++ tree/tmux.sh
@@ -2,11 +2,11 @@
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ "screen" ]] && [[ ! "$TERM" =~ "tmux" ]] && [ -z "$TMUX" ]
 then
     tmux attach || {
-        tmux new-session -d -x $(tput cols) -y $(tput lines) -s default -n org "echo; neofetch; bash";
-        tmux split-window -h -l 67 -t default "sh $HOME/.when/update_calendar.sh";
-        tmux split-window -v -t default "sh $HOME/.todo/update_todo.sh";
+        tmux new-session -d -x $(tput cols) -y $(tput lines) -s default-$(whoami) -n org "echo; neofetch; bash";
+        tmux split-window -h -l 67 -t default-$(whoami) "sh $HOME/.when/update_calendar.sh";
+        tmux split-window -v -t default-$(whoami) "sh $HOME/.todo/update_todo.sh";
         tmux select-pane -L
-        shopt -q login_shell && tmux attach -t default || exec tmux attach -t default;
+        shopt -q login_shell && tmux attach -t default-$(whoami) || exec tmux attach -t default-$(whoami);
     }
     # } && shopt -q login_shell || exit
 fi
