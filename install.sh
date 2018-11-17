86bf761a38a5483de984515ebefbca4e27880716
ivanp7 <ivanp7.git@gmail.com>
2018-11-17 04:19:44 +0300

    Rename modules to objects

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@ CONF_DIR=$(realpath `dirname $0`)
 case $1 in
     "")
         echo "Installing everything:"
-        for module in $(find $CONF_DIR/modules/ -mindepth 2 -maxdepth 2 -type f -name install.sh)
+        for object in $(find $CONF_DIR/objects/ -mindepth 2 -maxdepth 2 -type f -name install.sh)
         do
-            echo "Installing module '$(basename $(dirname $module))'..."
-            $module
-            # echo $module
+            echo "Installing object '$(basename $(dirname $object))'..."
+            $object
+            # echo $object
         done
         echo "Done!"
         ;;
 
     *)
-        if [ ! -x $CONF_DIR/modules/$1/install.sh ]
-        then echo "Error: invalid module '$1'."; exit 1
+        if [ ! -x $CONF_DIR/objects/$1/install.sh ]
+        then echo "Error: invalid object '$1'."; exit 1
         else
-            echo "Installing module '$1'..."
-            $CONF_DIR/modules/$1/install.sh
+            echo "Installing object '$1'..."
+            $CONF_DIR/objects/$1/install.sh
             echo "Done!"
         fi
 esac
