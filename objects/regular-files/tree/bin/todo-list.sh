eee67d99c9537bf9ae68215038230a131ae1683d
ivanp7 <ivanp7.git@gmail.com>
2018-11-19 17:53:32 +0300

    Add check for directory existence in todo-list.sh

--- objects/regular-files/tree/bin/todo-list.sh
+++ objects/regular-files/tree/bin/todo-list.sh
@@ -5,9 +5,12 @@ OLD_LIST=$(basename $(readlink $HOME/Org/todo))
 if [[ -z "$1" ]]
 then
     echo $OLD_LIST
-else
+elif [[ -d "$HOME/Org/todo-lists/$1" ]]
+then
     rm $HOME/Org/todo
     ln -sf todo-lists/$1 $HOME/Org/todo
     touch $HOME/Org/todo-lists/$OLD_LIST/todo.txt
+else
+    echo "No such TO-DO list '$1'."
 fi
 
