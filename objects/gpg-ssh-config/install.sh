6fe86a174ad74dfc67aa3da524c7b198c8399d95
ivanp7 <ivanp7.git@gmail.com>
2019-02-25 15:09:58 +0300

    Move GnuPG configuration out of regular-files to allow permissions setting

---
+++ objects/gpg-ssh-config/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONF_DIR=$(realpath `dirname $0`)
+
+mkdir -p $HOME/.ssh/
+chmod 700 $HOME/.ssh/
+cp -f $CONF_DIR/config $HOME/.ssh/
+chmod 644 $HOME/.ssh/config
+
+mkdir -p $HOME/.gnupg/
+chmod 700 $HOME/.gnupg/
+cp -f $CONF_DIR/gpg.conf $CONF_DIR/gpg-agent.conf $HOME/.gnupg/
+chmod 644 $HOME/.gnupg/gpg.conf $HOME/.gnupg/gpg-agent.conf
+
--- objects/ssh-config/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONF_DIR=$(realpath `dirname $0`)
-
-mkdir -p $HOME/.ssh/
-chmod 700 $HOME/.ssh/
-cp -f $CONF_DIR/config $HOME/.ssh/
-chmod 644 $HOME/.ssh/config
-
