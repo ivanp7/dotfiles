df11370e89431615ae1a7505e36e49fa760350bd
ivanp7 <ivanp7.git@gmail.com>
2019-02-25 16:37:12 +0300

    Change SSH and GnuPG configuration files permissions to 600

--- objects/gpg-ssh-config/install.sh
+++ objects/gpg-ssh-config/install.sh
@@ -5,10 +5,10 @@ CONF_DIR=$(realpath `dirname $0`)
 mkdir -p $HOME/.ssh/
 chmod 700 $HOME/.ssh/
 cp -f $CONF_DIR/config $HOME/.ssh/
-chmod 644 $HOME/.ssh/config
+chmod 600 $HOME/.ssh/config
 
 mkdir -p $HOME/.gnupg/
 chmod 700 $HOME/.gnupg/
 cp -f $CONF_DIR/gpg.conf $CONF_DIR/gpg-agent.conf $HOME/.gnupg/
-chmod 644 $HOME/.gnupg/gpg.conf $HOME/.gnupg/gpg-agent.conf
+chmod 600 $HOME/.gnupg/gpg.conf $HOME/.gnupg/gpg-agent.conf
 
