4aa90347557697a4d9d3c0866ec762366844ecf4
ivanp7 <ivanp7.git@gmail.com>
2019-03-02 16:37:00 +0300

    Remove issue-picture.sh script; re-add remote.sh script

--- objects/regular-files/tree/bin/issue-picture.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat | sudo tee /etc/issue > /dev/null
-echo | sudo tee -a /etc/issue > /dev/null
-echo 'Arch Linux \r (\l)' | sudo tee -a /etc/issue > /dev/null
-echo | sudo tee -a /etc/issue > /dev/null
-
---
+++ objects/remote-sh-hosts/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CONF_DIR=$(realpath `dirname $0`)
+
+touch $HOME/.ssh_known_hosts
+
