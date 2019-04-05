b77c49941b9d700431f8cd71d983aeaa65050d79
ivanp7 <ivanp7.git@gmail.com>
2019-04-06 01:50:45 +0300

    Remove .motd_pic so it can be customized per computer

--- objects/shell-scripts/scripts/print-motd.sh
+++ objects/shell-scripts/scripts/print-motd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ $(tput colors) -ge 256 ]] && pixterm -tc $(tput cols) -tr $(($(tput lines) / 2)) $HOME/.motd_pic
+[[ $(tput colors) -ge 256 ]] && [[ -f $HOME/.motd_pic ]] && pixterm -tc $(tput cols) -tr $(($(tput lines) / 2)) $HOME/.motd_pic
 echo
 echo
 echo
