b597648acdd6a84c6fff116f2bdd3c4116e5b7bb
ivanp7 <ivanp7.git@gmail.com>
2019-04-11 03:00:02 +0300

    Remove user info from .gitconfig

--- objects/git-config/install.sh
+++ objects/git-config/install.sh
@@ -3,6 +3,5 @@
 CONF_DIR=$(realpath `dirname $0`)
 
 install -Dm 644 $CONF_DIR/.gitconfig $HOME/
-sed -i "s/USERNAME/$(whoami)/g" $HOME/.gitconfig
-sed -i "s@HOME@$HOME@g" $HOME/.gitconfig
+install -Dm 644 $CONF_DIR/.gitignore_global $HOME/
 
