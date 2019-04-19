24891ecbfb950ecc689860e170be72f0835d7c99
ivanp7 <ivanp7.git@gmail.com>
2019-04-19 23:48:14 +0300

    Fix remote.sh autocompletion script

--- objects/symlinked-directories/tree/.zsh/completion/_remote.sh
+++ objects/symlinked-directories/tree/.zsh/completion/_remote.sh
@@ -3,7 +3,7 @@
 _remote_sh ()
 {
     _arguments \
-        "1:Host:($(ls $HOME/.password-store/computers/))" \
+        "1:Host:($(ls $HOME/.password-store/computers/ 2> /dev/null))" \
         "2:Mode:(status wakeup upload wakeup-upload download wakeup-download mount wakeup-mount unmount command wakeup-command tunnel wakeup-tunnel)"
 }
 
