31bb1a3b297c7d4a8159770c7deeff5035a7a7e2
ivanp7 <ivanp7.git@gmail.com>
2019-04-22 22:00:14 +0300

    Change default tmux session name

--- objects/symlinked-files/tree/scripts/tmux-default-session.sh
+++ objects/symlinked-files/tree/scripts/tmux-default-session.sh
@@ -1,8 +1,10 @@
+TMUX_DEFAULT_SESSION_NAME=default
+
 # ORG_COLS=68
 ORG_COLS=100
 
-tmux new-session -d -x $(tput cols) -y $(tput lines) -s default-$(whoami) -n org "sh $HOME/.when/update_calendar.sh";
-tmux split-window -v -t default-$(whoami) "sh $HOME/.task/update_task.sh";
-tmux split-window -h -b -f -l $(( $(tput cols) - $ORG_COLS )) -t default-$(whoami) "$HOME/bin/df/print-motd.sh; $SHELL";
-[[ -o login ]] && tmux attach -t default-$(whoami) || exec tmux attach -t default-$(whoami);
+tmux new-session -d -x $(tput cols) -y $(tput lines) -s $TMUX_DEFAULT_SESSION_NAME -n org "sh $HOME/.when/update_calendar.sh"
+tmux split-window -v -t $TMUX_DEFAULT_SESSION_NAME "sh $HOME/.task/update_task.sh"
+tmux split-window -h -b -f -l $(( $(tput cols) - $ORG_COLS )) -t $TMUX_DEFAULT_SESSION_NAME "$HOME/bin/df/print-motd.sh; $SHELL"
+[[ -o login ]] && tmux attach -t $TMUX_DEFAULT_SESSION_NAME || exec tmux attach -t $TMUX_DEFAULT_SESSION_NAME
 
