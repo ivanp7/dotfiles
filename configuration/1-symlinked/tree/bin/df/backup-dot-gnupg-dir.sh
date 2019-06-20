05bf028c74009fda640c32d57fa7960e460ee1b2
ivanp7 <ivanp7.git@gmail.com>
2019-06-20 18:57:55 +0300

    Do not tar .gnupg at home, do it locally

--- configuration/1-symlinked/tree/bin/df/backup-dot-gnupg-dir.sh
+++ configuration/1-symlinked/tree/bin/df/backup-dot-gnupg-dir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tar -cvf .gnupg.tar --exclude="*.conf" -C $HOME/.gnupg . > /dev/null 2>&1
+tar -cvf .gnupg.tar --exclude="*.conf" -C ./.gnupg . > /dev/null 2>&1
 gpg --symmetric .gnupg.tar
 rm .gnupg.tar
 
