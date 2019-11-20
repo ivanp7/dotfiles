70073b6570b3453fc005c268808423e318c3c5f9
ivanp7 <ivanp7.git@gmail.com>
2019-11-20 20:42:10 +0300

    Convert gnupg backup script into a general directory backup script

--- configuration/1-symlinked/tree/bin/df/backup-dot-gnupg-dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-tar -cvf .gnupg.tar --exclude="*.conf" -C ./.gnupg . > /dev/null 2>&1
-gpg --symmetric .gnupg.tar
-rm .gnupg.tar
-
---
+++ configuration/1-symlinked/tree/bin/df/encrypt-directory.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+NAME=$(basename $(realpath $1))
+
+tar -cvf "$NAME.tar" $1 > /dev/null 2>&1
+gpg --armor --symmetric "$NAME.tar"
+rm "$NAME.tar"
+
