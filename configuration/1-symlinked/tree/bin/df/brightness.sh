6a307e7916f0af5959b0db5713f03fcd95f81bec
ivanp7 <ivanp7.git@gmail.com>
2019-12-19 21:36:33 +0300

    Rewrite brightness.sh

--- configuration/1-symlinked/tree/bin/df/brightness.sh
+++ configuration/1-symlinked/tree/bin/df/brightness.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-if [ ! -d /sys/class/backlight ]; then exit 1; fi
+BACKLIGHT=$(find /sys/class/backlight -type l 2> /dev/null | head -n1)
+if [ ! -d "$BACKLIGHT" ]; then exit 1; fi
 
-BACKLIGHT=$(basename $(find /sys/class/backlight -type l | head -n1))
+BRIGHTNESS=$(cat $BACKLIGHT/actual_brightness)
 
-echo Max brightness: $(cat /sys/class/backlight/$BACKLIGHT/max_brightness)
-if [ -n "$1" ]; then echo $1 | sudo tee /sys/class/backlight/$BACKLIGHT/brightness > /dev/null; fi
-echo Current brighness: $(cat /sys/class/backlight/$BACKLIGHT/actual_brightness)
+MAX_BRIGHTNESS=$(cat $BACKLIGHT/max_brightness)
+STEP=$(($MAX_BRIGHTNESS / 10))
+
+case "$1" in
+    "") echo "$BRIGHTNESS/$MAX_BRIGHTNESS" ;;
+    up) echo $(($BRIGHTNESS + $STEP)) > $BACKLIGHT/brightness ;;
+    down) echo $(($BRIGHTNESS - $STEP)) > $BACKLIGHT/brightness ;;
+    *) echo "$1" > $BACKLIGHT/brightness ;;
+esac
 
