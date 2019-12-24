eb74cc717bf21ecd1e80d60e05daeb7a3c476c9f
ivanp7 <ivanp7.git@gmail.com>
2019-12-24 22:50:29 +0300

    Rewrite extdev.sh and split it into 2 scripts

---
+++ configuration/1-symlinked/tree/bin/df/extmount.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+DEVICE="$1"
+
+if [ -z "$DEVICE" ]
+then lsblk -lp | grep "part $" | awk '{print $1 " (" $4 ")"}'; exit 0
+fi
+
+sudo mount "$DEVICE" 2> /dev/null && exit 0
+
+MOUNTPOINT="$2"
+
+if [ -z "$MOUNTPOINT" ]
+then MOUNTPOINT="/mnt/dev/$(basename $DEVICE)"
+fi
+
+if mountpoint -q "$MOUNTPOINT"
+then echo "Error: directory '$MOUNTPOINT' is a mountpoint already."; exit 1
+fi
+
+sudo mkdir -p "$MOUNTPOINT"
+
+MOUNT_OPTIONS="$3"
+MOUNT_OPTIONS_EXTRA="$4"
+
+if [ -z "$MOUNT_OPTIONS" ]
+then MOUNT_OPTIONS="defaults,rw,uid=$(id -u),gid=$(id -g),dmask=0022,fmask=0133"
+fi
+
+if [ -n "$MOUNT_OPTIONS_EXTRA" ]
+then MOUNT_OPTIONS+=",$MOUNT_OPTIONS_EXTRA"
+fi
+
+sudo mount -o "$MOUNT_OPTIONS" "$DEVICE" "$MOUNTPOINT"
+
---
+++ configuration/1-symlinked/tree/bin/df/extunmount.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+EXCLUSIONS="\(/\|/boot\|/home\)$"
+
+DEVICE="$1"
+
+if [ -z "$DEVICE" ]
+then 
+    lsblk -lp | grep "part /" | grep -v "$EXCLUSIONS" | 
+        awk '{print $1 " (" $4 ") @ " $7}'
+    exit 0
+fi
+
+sudo umount "$DEVICE"
+
