e9165530ed9d16d0c253b5186c10286017ac9677
ivanp7 <ivanp7.git@gmail.com>
2020-01-17 22:15:06 +0300

    Improve background image selection command

--- 1-symlinked/tree/bin/df/y-desktop.sh
+++ 1-symlinked/tree/bin/df/y-desktop.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-WALLPAPER=$(find -L $HOME/wallpapers/ -name "*.pic" | shuf -n1)
+WALLPAPER="$(find -L $HOME/wallpapers/ -type f -o -type l | 
+    while read file
+    do
+        if xdg-mime query filetype "$file" | grep -q "^image/"
+        then echo "$file"
+        fi
+    done | shuf -n1)"
 
 if [ -n "$WALLPAPER" ]
 then screen -c $HOME/.screenrc_noui yaft_wall $WALLPAPER;
