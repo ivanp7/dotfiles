8c0a1af38533976ba3ac5455cf0b97908c9a5f9e
ivanp7 <ivanp7.git@gmail.com>
2020-05-06 16:59:28 +0300

    Add minor improvements

--- 1-symlinked/tree/bin/df/encrypt-directory.sh
+++ 1-symlinked/tree/bin/df/encrypt-directory.sh
@@ -2,7 +2,7 @@
 
 NAME="$(basename "$(realpath "$1")")"
 
-tar -cvf "$NAME.tar" $1 > /dev/null 2>&1
+tar -cvf "$NAME.tar" "$1" > /dev/null 2>&1
 gpg --armor --symmetric "$NAME.tar"
 rm "$NAME.tar"
 
