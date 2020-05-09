d7991728543ea9ce3fed03c7a081471549b556ff
ivanp7 <ivanp7.git@gmail.com>
2020-05-10 02:53:18 +0300

    Use image2text for image previews in lf and preview.sh

--- 1-symlinked/tree/.scripts/df/preview-video-thumbnail.sh
+++ 1-symlinked/tree/.scripts/df/preview-video-thumbnail.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CACHE=$(mktemp /tmp/thumb_cache.XXXXX)
-ffmpegthumbnailer -i "$1" -o $CACHE -s 0
+CACHE=$(mktemp /tmp/thumb_cache.XXXXX.png)
+ffmpegthumbnailer -i "$1" -o $CACHE -s 0 -c png
 $(dirname "$0")/preview-image.sh $CACHE && rm $CACHE
 
