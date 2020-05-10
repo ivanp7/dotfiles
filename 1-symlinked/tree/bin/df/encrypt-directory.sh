787782d8bc710f9809718bfe78158148c6507646
ivanp7 <ivanp7.git@gmail.com>
2020-05-11 01:55:10 +0300

    Add support for symmetric encryption/decryption operations in lf

--- 1-symlinked/tree/bin/df/encrypt-directory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-NAME="$(basename "$(realpath "$1")")"
-
-tar -cvf "$NAME.tar" "$1" > /dev/null 2>&1
-gpg --armor --symmetric "$NAME.tar"
-rm "$NAME.tar"
-
--- 1-symlinked/tree/bin/df/extract.sh
+++ 1-symlinked/tree/bin/df/extract.sh
@@ -1,44 +1,42 @@
 #!/bin/sh
 # (xkcd link: https://xkcd.com/1168/)
 
+FILE="$1"
 POSTFIX="-archive-contents"
 
-for file in "$@"
-do
-    mime_type=$(file --mime-type "$file" -bLE) || mime_type=""
-    case $mime_type in
-        application/*)
-            case $(echo $mime_type | tail -c +13) in
-                x-tar|x-bzip|x-bzip2|gzip|x-xz|zstd|zip|x-zip-compressed|x-7z-compressed|x-iso9660-image|vnd.rar) ;;
-                *) continue
-            esac
-            ;;
-        *) continue
-    esac
+mime_type=$(file --mime-type "$FILE" -bLE) || mime_type=""
+case $mime_type in
+    application/*)
+        case $(echo $mime_type | tail -c +13) in
+            x-tar|x-bzip|x-bzip2|gzip|x-xz|zstd|zip|x-zip-compressed|x-7z-compressed|x-iso9660-image|vnd.rar) ;;
+            *) continue
+        esac
+        ;;
+    *) continue
+esac
 
-    contents_dir="$(basename "$file")$POSTFIX"
-    mkdir "$contents_dir" || continue
-    cd "$contents_dir"
-    file="$(realpath "$file")"
+contents_dir="$(basename "$FILE")$POSTFIX"
+mkdir "$contents_dir" || continue
+cd "$contents_dir"
+FILE="$(realpath "$FILE")"
 
-    case $(echo $mime_type | tail -c +13) in
-        x-tar)
-            tar xvf "$file" ;;
-        x-bzip|x-bzip2)
-            tar xvf "$file" -j ;;
-        gzip)
-            tar xvf "$file" -z ;;
-        x-xz)
-            tar xvf "$file" -J ;;
-        zstd)
-            tar xvf "$file" --zstd ;;
-        zip|x-zip-compressed)
-            unzip "$file" ;;
-        x-7z-compressed|x-iso9660-image)
-            7z x "$file" ;;
-        vnd.rar)
-            unrar x "$file" ;;
-    esac
-    cd ..
-done
+case $(echo $mime_type | tail -c +13) in
+    x-tar)
+        tar xvf "$FILE" ;;
+    x-bzip|x-bzip2)
+        tar xvf "$FILE" -j ;;
+    gzip)
+        tar xvf "$FILE" -z ;;
+    x-xz)
+        tar xvf "$FILE" -J ;;
+    zstd)
+        tar xvf "$FILE" --zstd ;;
+    zip|x-zip-compressed)
+        unzip "$FILE" ;;
+    x-7z-compressed|x-iso9660-image)
+        7z x "$FILE" ;;
+    vnd.rar)
+        unrar x "$FILE" ;;
+esac
+cd ..
 
