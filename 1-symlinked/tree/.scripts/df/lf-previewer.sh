042669d30ecff6ff648ad3884f4e05ac2a63d475
ivanp7 <ivanp7.git@gmail.com>
2020-05-10 16:02:17 +0300

    Add custom previewer for lf

---
+++ 1-symlinked/tree/.scripts/df/lf-previewer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+FILE="$1"
+COLUMNS=$2
+ROWS=$(($(tput lines) - 4))
+
+mime_type=$(file --mime-type "$FILE" -bLE) || mime_type=""
+case $mime_type in
+    text/*) head -n $ROWS "$FILE" ;;
+    *) echo "\033[0;7mBinary file of type '$mime_type'\033[0m"
+esac
+
