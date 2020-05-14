a2cee3787ec16fc79b6c865117df1cb746942d49
ivanp7 <ivanp7.git@gmail.com>
2020-05-14 16:44:47 +0300

    Add process tree display command to lf

---
+++ 1-symlinked/tree/.scripts/df/pstree.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+pstree -ls $$ | head -n 1 | sed "
+    s/-.-/---/g; 
+    s/systemd//;
+    s/---login//;
+    s/---pstree\.sh//;
+    s/---head//;
+    s/---sh//g;
+    s/---dash//g;
+    s/---zsh//g;
+    s/---bash//g;
+    s/---startx//g;
+    s/---xinit//g;
+    s/---dwm//g;
+    s/---x-desktop\.sh//g;
+    s/---dropdown-termin//g;
+    s/^---//;
+    s/sshd---sshd/ssh/;
+    s/tmux: server/tmux/;
+    s/screen---screen/screen/;
+    s/y-desktop\.sh---screen---yaft/yaft/;
+"
+
