e60d42982bd43538acc9983dbbaed430009a3696
ivanp7 <ivanp7.git@gmail.com>
2020-05-14 15:24:18 +0300

    Add several improvements and fixes

--- 9-special/instructions/vim.sh
+++ 9-special/instructions/vim.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+curl -fLo $HOME/.config/vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 mkdir -p $HOME/.cache/vim/plugged
 mkdir -p $HOME/.cache/vim/undo
 mkdir -p $HOME/.cache/vim/swap
