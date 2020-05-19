13a31cc103041ff0cd74dead045b0cf3b317ce47
ivanp7 <ivanp7.git@gmail.com>
2020-05-19 13:44:57 +0300

    Add support for gnupg pinentry wrapper script

--- 9-special/instructions/gnupg.sh
+++ 9-special/instructions/gnupg.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sed -i "s/USERNAME/$USER/g" $HOME/.local/share/gnupg/gpg-agent.conf
+
 chmod 700 $HOME/.local/share/gnupg/
 chmod 600 $HOME/.local/share/gnupg/gpg.conf
 chmod 600 $HOME/.local/share/gnupg/gpg-agent.conf 
