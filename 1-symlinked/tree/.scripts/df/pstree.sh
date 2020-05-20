0535cea8587dc2ad7991eb7a3fa53d80a2c8ee39
ivanp7 <ivanp7.git@gmail.com>
2020-05-20 16:51:06 +0300

    Optimize pstree.h

--- 1-symlinked/tree/.scripts/df/pstree.sh
+++ 1-symlinked/tree/.scripts/df/pstree.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 
 pstree -ls $$ | head -n 1 | sed -E "
-    s/-.-/---/g; 
-    s/systemd//;
-    s/---login//;
-    s/---pstree\.sh//;
-    s/---head//;
-    s/---sh//g;
-    s/---dash//g;
-    s/---zsh//g;
-    s/---bash//g;
-    s/---startx//g;
-    s/---xinit//g;
-    s/---dwm//g;
-    s/---x-desktop\.sh//g;
-    s/---dropdown-termin//g;
+    s/-(-|[+])-/---/g; 
+    s/^(runit|systemd)(|---runsvdir---runsv)(|---login)//;
+    s/---pstree\.sh---head$//;
+    s/---(|z|ba|da)sh//g;
     s/^---//;
-    s/sshd(---sshd)*/ssh/;
-    s/tmux: server/tmux/;
-    s/screen(---screen)*/screen/;
+    s/sshd(---sshd)*/ssh/g;
+    s/tmux: server/tmux/g; s/screen(---screen)*/screen/g;
     s/y-desktop\.sh---screen---yaft/yaft/;
 "
 
