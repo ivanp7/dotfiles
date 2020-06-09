3e0a2e306a2156d79c158c3ff3c233d7e7e562f0
ivanp7 <ivanp7.git@gmail.com>
2020-06-09 21:32:10 +0300

    Improve lf previewer script

--- 1-symlinked/tree/.scripts/df/lf-previewer.sh
+++ 1-symlinked/tree/.scripts/df/lf-previewer.sh
@@ -8,6 +8,6 @@ mime_type=$(file --mime-type "$FILE" -bLE) || mime_type=""
 case $mime_type in
     ""|inode/x-empty) ;;
     text/*) head -n $ROWS "$FILE" ;;
-    *) echo "\033[0;7mBinary file of type '$mime_type'\033[0m"
+    *) echo "\033[0;7m$(file -bLE "$FILE")\033[0m"
 esac
 
