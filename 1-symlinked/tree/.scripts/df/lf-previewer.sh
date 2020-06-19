d101339dd9229a15b6dbaabc888f49d2893c5efb
ivanp7 <ivanp7.git@gmail.com>
2020-06-19 13:13:09 +0300

    Display JSON file contents in lf previewer

--- 1-symlinked/tree/.scripts/df/lf-previewer.sh
+++ 1-symlinked/tree/.scripts/df/lf-previewer.sh
@@ -7,7 +7,7 @@ ROWS=$(($(tput lines) - 4))
 mime_type=$(file --mime-type "$FILE" -bLE) || mime_type=""
 case $mime_type in
     ""|inode/x-empty) ;;
-    text/*) head -n $ROWS "$FILE" ;;
+    text/*|application/json) head -n $ROWS "$FILE" ;;
     *) echo "\033[0;7m$(file -bLE "$FILE")\033[0m"
 esac
 
