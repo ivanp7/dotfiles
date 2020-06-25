051d3235b51429b3eb331f386fd1a6a39c90d280
ivanp7 <ivanp7.git@gmail.com>
2020-06-25 14:31:02 +0300

    Don't autostart Glances in tmux session

--- 1-symlinked/tree/.scripts/df/tmux-default-session.sh
+++ 1-symlinked/tree/.scripts/df/tmux-default-session.sh
@@ -6,8 +6,7 @@ HEIGHT=$(tput lines)
 # ORG_COLS=68
 ORG_COLS=80
 
-tmux new-session -d -x $WIDTH -y $HEIGHT -s $TMUX_DEFAULT_SESSION_NAME -n monitor "glances"
-tmux new-window -n org "display-calendar.sh"
+tmux new-session -d -x $WIDTH -y $HEIGHT -s $TMUX_DEFAULT_SESSION_NAME -n org "display-calendar.sh"
 tmux split-window -v -t $TMUX_DEFAULT_SESSION_NAME "display-tasks.sh"
 tmux split-window -h -b -f -l $(($WIDTH - $ORG_COLS)) -t $TMUX_DEFAULT_SESSION_NAME "$HOME/.scripts/df/tmux-motd.sh; $SHELL"
 task > /dev/null 2>&1
