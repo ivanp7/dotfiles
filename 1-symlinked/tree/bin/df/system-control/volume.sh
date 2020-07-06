5bea77c4ceb27b0df7f931619d1a1f25d2580ab5
ivanp7 <ivanp7.git@gmail.com>
2020-07-07 02:05:13 +0300

    Add minor improvements to code style

--- 1-symlinked/tree/bin/df/system-control/volume.sh
+++ 1-symlinked/tree/bin/df/system-control/volume.sh
@@ -1,53 +1,40 @@
 #!/bin/sh
 
-state ()
+MASTER=$(amixer -M sget Master)
+
+state_level ()
 {
-    amixer sget Master | sed -E "/.*$1:.*\[(on|off)\].*/!d;s//\1/" 2> /dev/null
+    echo "$MASTER" | sed -E "/\s*$1:.*\[([0-9]+)%\].*\[(on|off)\].*/!d; s//\2 \1/" 2> /dev/null
 }
 
-level ()
-{
-    amixer -M sget Master | sed -E "/.*$1:.*\[(.*)%\].*/!d;s//\1/" 2> /dev/null
-}
-
-STATE=$(state "Mono")
-LEVEL=$(level "Mono")
+STATE_LEVEL=$(state_level "Mono")
+STATE=${STATE_LEVEL% *}
+LEVEL=${STATE_LEVEL#* }
 
 if [ -z "$STATE" ]
 then
-    STATE_LEFT=$(state "Front Left")
-    STATE_RIGHT=$(state "Front Right")
-    if [ "$STATE_LEFT" = "on" ] || [ "$STATE_RIGHT" = "on" ]
-    then STATE="on"
-    else STATE="off"
-    fi
-    LEVEL_LEFT=$(level "Front Left")
-    LEVEL_RIGHT=$(level "Front Right")
+    STATE_LEVEL_LEFT=$(state_level "Front Left")
+    STATE_LEVEL_RIGHT=$(state_level "Front Right")
+
+    STATE_LEFT=${STATE_LEVEL_LEFT% *}
+    STATE_RIGHT=${STATE_LEVEL_RIGHT% *}
+    [ "$STATE_LEFT" = "on" -o "$STATE_RIGHT" = "on" ] && STATE="on" || STATE="off"
+
+    LEVEL_LEFT=${STATE_LEVEL_LEFT#* }
+    LEVEL_RIGHT=${STATE_LEVEL_RIGHT#* }
     LEVEL=$(( ($LEVEL_LEFT + $LEVEL_RIGHT) / 2 ))
 fi
 
 case $1 in
     "") echo "$LEVEL% $STATE" ;;
     toggle) 
-        if [ "$STATE" = "on" ]
-        then
-            amixer -q sset Master mute
-            amixer -q sset Speaker mute 2> /dev/null
-            amixer -q sset Headphone mute 2> /dev/null
-        elif [ "$STATE" = "off" ]
-        then
-            amixer -q sset Master unmute
-            amixer -q sset Speaker unmute 2> /dev/null
-            amixer -q sset Headphone unmute 2> /dev/null
-        fi
+        case "$STATE" in
+            on) amixer -q sset Master mute ;;
+            off) amixer -q sset Master unmute ;;
+        esac
         ;;
     up) amixer -q -M sset Master 3%+ ;;
     down) amixer -q -M sset Master 3%- ;;
-    calibrate)
-        amixer -q -M sset Speaker 100% 2> /dev/null
-        amixer -q -M sset Headphone 100% 2> /dev/null
-        ;;
     *) amixer -q -M sset Master $1%
 esac
-
 
