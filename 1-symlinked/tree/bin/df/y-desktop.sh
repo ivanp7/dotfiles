1f4643e0d7ca111dcff2426d8bc4ec1aab8d65fc
ivanp7 <ivanp7.git@gmail.com>
2020-07-19 15:33:49 +0300

    Move wallpaper pictures to a separate repository

--- 1-symlinked/tree/bin/df/y-desktop.sh
+++ 1-symlinked/tree/bin/df/y-desktop.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-WALLPAPER="$(find -L $HOME/wallpapers/ -type f -o -type l | 
-    while read file
-    do file -b -i "$file" | grep -q "^image/" && echo "$file"
-    done | shuf -n 1)"
+WALLPAPER=$HOME/wallpapers/yaft
 
-if [ -n "$WALLPAPER" ]
+if [ -e "$WALLPAPER" ] && file -b -i "$WALLPAPER" | grep -q "^image/"
 then screen -c $XDG_CONFIG_HOME/screen/screenrc_noui yaft_wall "$WALLPAPER";
 else screen -c $XDG_CONFIG_HOME/screen/screenrc_noui yaft;
 fi
