3c8c15d79433132a621bfad6ad85dd6951b74ca6
ivanp7 <ivanp7.git@gmail.com>
2020-07-22 12:10:57 +0300

    Improve tmux battery info script

--- 1-symlinked/tree/.scripts/df/tmux-battery-info.sh
+++ 1-symlinked/tree/.scripts/df/tmux-battery-info.sh
@@ -3,7 +3,7 @@
 DELIMITER=$1
 
 acpi -b 2> /dev/null | sed -E "
-s/^Battery /bat/; s/ remaining\$//; s/ at zero rate.*//;
+s/^Battery /bat/; s/ remaining\$//; s/ at zero rate.*//; s/ until charged\$//;
 s/Discharging,/↓/; s/Charging,/↑/; s/Unknown, //;
 s/ ([0-9]+%), (\S+)/ \1 \(\2\)/;
 s/^/ /; s/\$/ $DELIMITER/" | tr -d '\n' | sed "s/^  $DELIMITER\$//"
