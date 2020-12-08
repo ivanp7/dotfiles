b250f384defe52a8a8add9858f7d59b21c17cfb9
ivanp7 <ivanp7@mail.ru>
2020-12-09 00:38:42 +0300

    Translate <br> to line break in dict.sh

--- 1-symlinked/tree/bin/df/applications/dict.sh
+++ 1-symlinked/tree/bin/df/applications/dict.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec sdcv --utf8-input --utf8-output --color "$@" | sed 's/\([[:digit:]]\+)\)/\n\n\1/g; s/;/;\n/g;'
+exec sdcv --utf8-input --utf8-output --color "$@" | sed 's/\([[:digit:]]\+)\)/\n\n\1/g; s/;/;\n/g; s/<br>/\n/g'
 
