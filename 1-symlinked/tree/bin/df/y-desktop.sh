3f2a4d7eaf7d86470ca51445a70a3a219ddd3293
ivanp7 <ivanp7@mail.ru>
2021-03-16 13:00:52 +0300

    Display shell info after exiting from yaft

--- 1-symlinked/tree/bin/df/y-desktop.sh
+++ 1-symlinked/tree/bin/df/y-desktop.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# request display of shell info
+PIDFILE="$TMPDIR_CURRENT/shell_info$(tty)/$PARENT_SHELL_PID_EXPORTED"
+[ -n "$PARENT_SHELL_PID_EXPORTED" -a -f "$PIDFILE" ] && rm "$PIDFILE" || true
+
 WALLPAPER=$HOME/wallpapers/yaft
 
 if [ -e "$WALLPAPER" ] && file -b -i "$WALLPAPER" | grep -q "^image/"
