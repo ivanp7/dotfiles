72882857a40f22261d9e58f0074c539b366e5130
ivanp7 <ivanp7@mail.ru>
2021-03-18 20:52:38 +0300

    Minor style improvements

--- 1-symlinked/tree/bin/df/y-desktop.sh
+++ 1-symlinked/tree/bin/df/y-desktop.sh
@@ -7,8 +7,8 @@ PIDFILE="$TMPDIR_CURRENT/shell_info$(tty)/$PARENT_SHELL_PID_EXPORTED"
 WALLPAPER=$HOME/wallpapers/yaft
 
 if [ -e "$WALLPAPER" ] && file -b -i "$WALLPAPER" | grep -q "^image/"
-then screen -c $XDG_CONFIG_HOME/screen/screenrc_noui yaft_wall "$WALLPAPER";
-else screen -c $XDG_CONFIG_HOME/screen/screenrc_noui yaft;
+then screen -c "$XDG_CONFIG_HOME/screen/screenrc_noui" yaft_wall "$WALLPAPER";
+else screen -c "$XDG_CONFIG_HOME/screen/screenrc_noui" yaft;
 fi
 
 clear
