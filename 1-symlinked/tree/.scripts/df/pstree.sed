3ec6cff8efc04834100c197ac86230d428d5fdba
ivanp7 <ivanp7@mail.ru>
2021-05-19 13:59:34 +0300

    Replace yaft with fbterm

--- 1-symlinked/tree/.scripts/df/pstree.sed
+++ 1-symlinked/tree/.scripts/df/pstree.sed
@@ -3,5 +3,3 @@ s/^---//;
 s/---pstree$//;
 s/sshd(---sshd)*/(ssh)/g;
 s/tmux: server/(tmux)/g; s/screen(---screen)*/(screen)/g;
-s/y-desktop\.sh---\(screen\)---yaft/(yaft)/;
-
---
+++ 1-symlinked/tree/bin/df/fbterm.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+[ "$TERM" = "linux" ] || exit
+
+# request display of shell info
+PIDFILE="$TMPDIR_CURRENT/shell_info$(tty)/$PARENT_SHELL_PID_EXPORTED"
+[ -n "$PARENT_SHELL_PID_EXPORTED" -a -f "$PIDFILE" ] && rm "$PIDFILE" || true
+
+WALLPAPER="$HOME/wallpapers/fbterm"
+
+if [ -e "$WALLPAPER" ] && file --mime-type "$WALLPAPER" -bLE | grep -q "^image/"
+then
+    echo -ne "\e[?25l" # hide cursor
+    fbv -ciuker "$WALLPAPER" << EOF
+q
+EOF
+    export FBTERM_BACKGROUND_IMAGE=1
+fi
+
+exec fbterm -n "$DEFAULT_FONT_NAME" -s "$DEFAULT_FONT_SIZE" -- "$HOME/.scripts/df/fbterm.sh" "$1"
+
